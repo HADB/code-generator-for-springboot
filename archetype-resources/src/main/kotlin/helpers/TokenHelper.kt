91eff910b92e6dbe8863ca3b893dc699957d6f9e
Bean <bean@yuanfen.net>
2020-05-06 10:41:04 +0800

    优化 TokenHelper

--- archetype-resources/src/main/kotlin/helpers/TokenHelper.kt
+++ archetype-resources/src/main/kotlin/helpers/TokenHelper.kt
@@ -13,36 +13,36 @@ class TokenHelper {
     private lateinit var redisHelper: RedisHelper
 
     // 创建 Token 与 Key 的双向映射
-    fun createToken(system: String?, key: String): String {
-        deleteToken(system, key) // 删除老Token
-        val token = "$${system ?: AppConstants.Service.DEFAULT}-$${UUID.randomUUID().toString().replace("-", "")}"
-        redisHelper.set(RedisKey.token(system, key), token, AppConstants.TOKEN_EXPIRES_HOUR, TimeUnit.HOURS)
-        redisHelper.set(RedisKey.key(system, token), key, AppConstants.TOKEN_EXPIRES_HOUR, TimeUnit.HOURS)
+    fun createToken(service: String?, key: String): String {
+        deleteToken(service, key) // 删除老Token
+        val token = UUID.randomUUID().toString().replace("-", "")
+        redisHelper.set(RedisKey.token(service, key), token, AppConstants.TOKEN_EXPIRES_HOUR, TimeUnit.HOURS)
+        redisHelper.set(RedisKey.key(service, token), key, AppConstants.TOKEN_EXPIRES_HOUR, TimeUnit.HOURS)
         return token
     }
 
     // 根据 Token 获取 Key
-    fun getTokenKey(system: String?, token: String): String? {
-        val key = redisHelper.get(RedisKey.key(system, token))
+    fun getTokenKey(service: String?, token: String): String? {
+        val key = redisHelper.get(RedisKey.key(service, token))
         if (key != null) {
-            updateToken(system, token, key) // 验证成功，延长 Token 的过期时间
+            updateToken(service, token, key) // 验证成功，延长 Token 的过期时间
         }
         return key
     }
 
     // 刷新 Token 和 Key 的双向映射
-    fun updateToken(system: String?, token: String, key: String) {
-        redisHelper.set(RedisKey.token(system, key), token, AppConstants.TOKEN_EXPIRES_HOUR, TimeUnit.HOURS)
-        redisHelper.set(RedisKey.key(system, token), key, AppConstants.TOKEN_EXPIRES_HOUR, TimeUnit.HOURS)
+    fun updateToken(service: String?, token: String, key: String) {
+        redisHelper.set(RedisKey.token(service, key), token, AppConstants.TOKEN_EXPIRES_HOUR, TimeUnit.HOURS)
+        redisHelper.set(RedisKey.key(service, token), key, AppConstants.TOKEN_EXPIRES_HOUR, TimeUnit.HOURS)
     }
 
     // 删除 Token
-    fun deleteToken(system: String?, key: String?) {
+    fun deleteToken(service: String?, key: String?) {
         if (key != null) {
-            val token = redisHelper.get(RedisKey.token(system, key))
+            val token = redisHelper.get(RedisKey.token(service, key))
             if (token != null) {
-                redisHelper.delete(RedisKey.token(system, key))
-                redisHelper.delete(RedisKey.key(system, token))
+                redisHelper.delete(RedisKey.token(service, key))
+                redisHelper.delete(RedisKey.key(service, token))
             }
         }
     }
