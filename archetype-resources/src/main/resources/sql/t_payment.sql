e73fdcb3cff2cd2dac4bfb8ec300b15d953ccb6b
Bean <bean@yuanfen.net>
2020-09-20 14:18:14 +0800

    update

--- archetype-resources/src/main/resources/sql/t_payment.sql
+++ archetype-resources/src/main/resources/sql/t_payment.sql
@@ -16,7 +16,7 @@ CREATE TABLE `t_payment` (
     `refund_time`           DATETIME            DEFAULT NULL            COMMENT '退款时间',
     `created_time`          DATETIME            DEFAULT NULL            COMMENT '创建时间',
     `updated_time`          DATETIME            DEFAULT NULL            COMMENT '更新时间',
-    `is_delete`             TINYINT(4)          NOT NULL DEFAULT '0'    COMMENT '是否删除(0:否, 1:是)',
+    `is_delete`             TINYINT(1)          NOT NULL DEFAULT '0'    COMMENT '是否删除(0:否, 1:是)',
     PRIMARY KEY (`id`),
     KEY         `idx_user_id`               (`user_id`),
     KEY         `idx_wx_transaction_id`     (`wx_transaction_id`),
