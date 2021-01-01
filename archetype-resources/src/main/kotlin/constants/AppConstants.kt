84d5f16d1ae4dde577d11e726e83aab7e070e1d1
Bean <bean@yuanfen.net>
2021-01-02 00:01:59 +0800

    优化 .gitlab-ci.yml

--- archetype-resources/src/main/kotlin/constants/AppConstants.kt
+++ archetype-resources/src/main/kotlin/constants/AppConstants.kt
@@ -4,7 +4,7 @@ class AppConstants {
     companion object {
         const val TOKEN = "token"
         const val SERVICE = "service"
-        const val REDIS_PREFIX = "op:${artifact_id}"
+        const val REDIS_PREFIX = "${artifact_id}"
         const val TOKEN_EXPIRES_HOUR = 24L
         const val AUTHENTICATION = "authentication"
         const val KEY = "key"
