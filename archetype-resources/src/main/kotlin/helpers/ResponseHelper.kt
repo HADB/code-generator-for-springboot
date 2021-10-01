2c3874e00fcf6115dcf0cd0294be1da95b8a6e8b
Bean <bean.deng@ximalaya.com>
2021-10-01 21:15:00 +0800

    rename

--- archetype-resources/src/main/kotlin/helpers/ResponseHelper.kt
+++ archetype-resources/src/main/kotlin/helpers/ResponseHelper.kt
@@ -9,13 +9,13 @@ import javax.servlet.http.HttpServletResponse
 @Component
 class ResponseHelper {
     @Resource
-    private lateinit var jacksonMapper: ObjectMapper
+    private lateinit var objectMapper: ObjectMapper
 
     fun setResponse(httpResponse: HttpServletResponse, response: Response<Any>) {
         httpResponse.characterEncoding = "UTF-8"
         httpResponse.contentType = "application/json"
         httpResponse.status = 200
-        httpResponse.writer.write(jacksonMapper.writeValueAsString(response))
+        httpResponse.writer.write(objectMapper.writeValueAsString(response))
         httpResponse.writer.flush()
     }
 }
