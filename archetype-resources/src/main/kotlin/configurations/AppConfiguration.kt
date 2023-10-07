5a12b473ead69f641d4db6e3801108a203b260c5
Bean <dengbin@bilibili.com>
2023-10-07 14:54:05 +0800

    feat: simplify AppConfiguration

--- archetype-resources/src/main/kotlin/configurations/AppConfiguration.kt
+++ archetype-resources/src/main/kotlin/configurations/AppConfiguration.kt
@@ -1,16 +1,14 @@
 package ${package_name}.configurations
 
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@ConfigurationProperties(prefix = "app")
 class AppConfiguration {
-    @Value("\$${app.qiniu.bucket}")
     lateinit var qiniuBucket: String
 
-    @Value("\$${app.qiniu.access-key}")
     lateinit var qiniuAccessKey: String
 
-    @Value("\$${app.qiniu.secret-key}")
     lateinit var qiniuSecretKey: String
 }
