f451ba2fbac2c472d2c6126df889c8b8440ac24e
Bean <dengbin@bilibili.com>
2023-12-12 17:05:27 +0800

    feat: add unique key for relationship tables

--- archetype-resources/src/main/resources/sql/t_user_role.sql
+++ archetype-resources/src/main/resources/sql/t_user_role.sql
@@ -5,10 +5,11 @@ CREATE TABLE `t_user_role` (
     `create_time`   DATETIME        DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
     `update_time`   DATETIME        DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
     `is_delete`     TINYINT         NOT NULL DEFAULT 0          COMMENT '是否删除',
-    PRIMARY KEY                     (`id`),
-    KEY         `idx_user_id`       (`user_id`),
-    KEY         `idx_role_id`       (`role_id`),
-    KEY         `idx_create_time`   (`create_time`),
-    KEY         `idx_update_time`   (`update_time`),
-    KEY         `idx_is_delete`     (`is_delete`)
+    PRIMARY KEY                         (`id`),
+    UNIQUE KEY  `idx_user_id_role_id`   (`user_id`, `role_id`),
+    KEY         `idx_user_id`           (`user_id`),
+    KEY         `idx_role_id`           (`role_id`),
+    KEY         `idx_create_time`       (`create_time`),
+    KEY         `idx_update_time`       (`update_time`),
+    KEY         `idx_is_delete`         (`is_delete`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户-角色';
